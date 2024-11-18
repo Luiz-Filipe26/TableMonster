9b3c555f2cab006152b523aed41e2262b22a4fdc
Luiz-Filipe-26 <luizferamos26@gmail.com>
2024-11-17 22:50:58 -0300

    Created executable and fixed atack and defense validation from CSV

--- src/Deck.kt
+++ src/Deck.kt
@@ -20,50 +20,50 @@ class Deck {
     }
 
     private fun readCardsFromCSV() {
-        val resourceURL = javaClass.getResource(filePath)
+        val resourceStream = javaClass.getResourceAsStream(filePath)
 
-        if (resourceURL == null) {
+        if (resourceStream == null) {
             println("Arquivo de cartas em ($filePath) não encontrado no classpath.")
             return
         }
 
-        val file = File(resourceURL.toURI())
+        resourceStream.bufferedReader().use { reader ->
+            reader.forEachLine { line ->
+                val tokens = line.split(";")
 
-        file.forEachLine { line ->
-            val tokens = line.split(";")
+                val name = tokens[0].trim()
+                if (name.isEmpty()) {
+                    println("Nome inválido: $name")
+                    return@forEachLine
+                }
 
-            val name = tokens[0].trim()
-            if (name.isEmpty()) {
-                println("Nome inválido: $name")
-                return@forEachLine
+                val description = tokens[1].trim()
+                if (description.isEmpty()) {
+                    println("Descrição inválida: $description")
+                    return@forEachLine
+                }
+
+                val attack = tokens[2].toIntOrNull()
+                if (attack == null || attack < 0) {
+                    println("Ataque inválido: ${tokens[2]}")
+                    return@forEachLine
+                }
+
+                val defense = tokens[3].toIntOrNull()
+                if (defense == null || defense < 0) {
+                    println("Defesa inválida: ${tokens[3]}")
+                    return@forEachLine
+                }
+
+                val cardType = CardType.fromString(tokens[4].trim())
+                if (cardType == null) {
+                    println("Tipo de carta desconhecido: ${tokens[4]}")
+                    return@forEachLine
+                }
+
+                val card = Card(name, description, cardType, attack, defense)
+                cardList.add(card)
             }
-
-            val description = tokens[1].trim()
-            if (description.isEmpty()) {
-                println("Descrição inválida: $description")
-                return@forEachLine
-            }
-
-            val attack = tokens[2].toIntOrNull()
-            if (attack == null || attack <= 0) {
-                println("Ataque inválido: ${tokens[2]}")
-                return@forEachLine
-            }
-
-            val defense = tokens[3].toIntOrNull()
-            if (defense == null || defense <= 0) {
-                println("Defesa inválida: ${tokens[3]}")
-                return@forEachLine
-            }
-
-            val cardType = CardType.fromString(tokens[4].trim())
-            if (cardType == null) {
-                println("Tipo de carta desconhecido: ${tokens[4]}")
-                return@forEachLine
-            }
-
-            val card = Card(name, description, cardType, attack, defense)
-            cardList.add(card)
         }
     }
 
